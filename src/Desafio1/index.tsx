6e4b0193b043b6c64fb111cec2940ca2fb0eb1a6
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-08 14:56:41 -0300

    Desafio1 resolvido

--- src/Desafio1/index.tsx
+++ src/Desafio1/index.tsx
@@ -3,12 +3,14 @@
   de errado e também a solução que você encontrou.
 
   Resposta / comentários sobre a sua solução:
-
+  Para renderizar um componente com mais de um elemento, os mesmos devem estar envoltos em um unico elemento. Para resolver esse problema utilizei o Fragment, assim mantem um codigo limpo sem a utilização de muitas div's.
 */
 
-export const Desafio1 = ()=> {
+export const Desafio1 = () => {
   return (
+    <>
       <span>item 1</span>
       <span>item 2</span>
+    </>
   )
 }
