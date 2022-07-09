983b6dadedc9c5af3b769411a65e65251bff2988
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-09 15:54:27 -0300

    Desafio 5 resolvido

--- src/Desafio5/Item/useItemController.ts
+++ src/Desafio5/Item/useItemController.ts
@@ -1,9 +1,16 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export const useItemController = () => {
   const [count, setCount] = useState<number>(0);
   const addCount = () => setCount(count + 1);
-  const renderCounter = useRef(0);
-  renderCounter.current += 1;
-  return { count, addCount, renderCounter: renderCounter.current };
+  const renderCounter = useRef(1);
+
+  useEffect(() => {
+    renderCounter.current += 1;
+  })
+
+  const isPar = (valor: number) => valor % 2 === 0;
+  const parOuImpar = (valor: number) => (isPar(valor) ? "Par" : "Impar");
+
+  return { count, addCount, renderCounter: renderCounter.current, parOuImpar };
 };
--- src/Desafio5/index.tsx
+++ src/Desafio5/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Item } from "./Item";
+import Item from "./Item";
 import { useDesafio5Controller } from "./useDesafio5Controller";
 
 /* TODO
@@ -8,22 +8,27 @@ import { useDesafio5Controller } from "./useDesafio5Controller";
   Obs. considere o componente filho também para resolver esse desafio
 
   Comentários sobre a sua solução:
-
+  Primeiramente para a função parOuImpar, a mesma foi adicionada no useItemController.ts, retirando das propriedades da const Item
+  par impar no item também.
+  Utilização do Reac.memo no componente Item para não renderizar novamente o componente e ele gravar(memorizar) o componente Item que ja estava renderizado.
+  Para o uso do useRef do contador de renderização, foi necessário usar useEffect para somar a contagem de renderização.
+  E por fim na linha 30 do componente Desafio5 foi necessário adicionar um espaço para igualar o teste.
 */
 
 export function Desafio5() {
   const { list, addItem, numero, parOuImpar } = useDesafio5Controller();
   return (
     <>
-      <button onClick={addItem}  data-testid="btn1">adiciona + 1 item na lista</button>
+      <button onClick={addItem} data-testid="btn1">adiciona + 1 item na lista
+      </button>
       <span data-testid="size">{list.length}</span>
       <ul>
-        {list.map((item) => (
-          <Item valor={item} parOuImpar={parOuImpar} />
+        {list.map((item, key) => (
+          <Item key={key} valor={item} />
         ))}
       </ul>
       <p>
-        {numero} é {parOuImpar(numero)}
+        {numero} é  {parOuImpar(numero)}
       </p>
     </>
   );
