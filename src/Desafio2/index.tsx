76fb8c7be5cd18713bbd6c80ce8e7005be00f6be
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-09 15:48:20 -0300

    Desafio 2 resolvido

--- src/Desafio2/index.tsx
+++ src/Desafio2/index.tsx
@@ -1,17 +1,21 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 /* TODO
   O componente abaixo está com problemas. Resolva o problema e explique o que tinha
   de errado e também a solução que você encontrou.
 
   Resposta / comentários sobre a sua solução:
-
+  O problema era a falta do atributo key para que o react possa identificar em qual item do array foi feita alguma alteração. Para solucionar o problema, foi adicionado ao método map o parâmetro de índice chamado de key, assim podendo ser utilizado para identificar corretamente cada item da renderização da lista.
 */
 
-export const Desafio2 = ()=> {
+export const Desafio2 = () => {
   const [list, setList] = useState([
     1, 2
   ]);
 
-  return (<ul>{list.map(item => <li>item: {item}</li>)}</ul>)
+  return (
+    <ul>
+      {list.map((item, key) => <li key={key}>item: {item}</li>)}
+    </ul>
+  )
 }
