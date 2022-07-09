cad17812bf4903cf9d0129a5e2842ffd6ceff0a8
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-09 15:57:59 -0300

    Desafios finalizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 
-import {Desafio1} from './Desafio1';
-import {Desafio2} from './Desafio2';
-import {Desafio3} from './Desafio3';
-import {Desafio4} from './Desafio4';
-import {Desafio5} from './Desafio5';
+import { Desafio1 } from './Desafio1';
+import { Desafio2 } from './Desafio2';
+import { Desafio3 } from './Desafio3';
+import { Desafio4 } from './Desafio4';
+import { Desafio5 } from './Desafio5';
 
 
 function App() {
--- src/__tests__/Desafio5.spec.tsx
+++ src/__tests__/Desafio5.spec.tsx
@@ -1,11 +1,11 @@
-import {Desafio5} from '../Desafio5';
-import {waitFor, render, getNodeText} from '@testing-library/react'
+import { Desafio5 } from '../Desafio5';
+import { waitFor, render, getNodeText } from '@testing-library/react'
 import user from '@testing-library/user-event'
-describe('Desafio5',()=>{
-  it('Deve ser Renderizado com os valores corretos 1',async ()=>{
-    const  component = render(<Desafio5 />)
-    const btn1 =await component.getByTestId('btn1')
-    const size =await component.getByTestId('size')
+describe('Desafio5', () => {
+  it('Deve ser Renderizado com os valores corretos 1', async () => {
+    const component = render(<Desafio5 />)
+    const btn1 = await component.getByTestId('btn1')
+    const size = await component.getByTestId('size')
     await user.click(btn1)
     await user.click(btn1)
     await user.click(btn1)
@@ -14,13 +14,13 @@ describe('Desafio5',()=>{
     await user.click(btn1)
     await user.click(btn1)
     await user.click(btn1)
-    await waitFor(()=>expect(getNodeText(size)).toEqual('8'))
+    await waitFor(() => expect(getNodeText(size)).toEqual('8'))
     expect(component).toMatchSnapshot()
   })
-  it('Deve ser Renderizado com os valores correto 2',async ()=>{
-    const  component = render(<Desafio5 />)
-    const btn1 =await component.getByTestId('btn1')
-    const size =await component.getByTestId('size')
+  it('Deve ser Renderizado com os valores correto 2', async () => {
+    const component = render(<Desafio5 />)
+    const btn1 = await component.getByTestId('btn1')
+    const size = await component.getByTestId('size')
     await user.click(btn1)
     await user.click(btn1)
     await user.click(btn1)
@@ -31,7 +31,7 @@ describe('Desafio5',()=>{
     await user.click(btn1)
     await user.click(btn1)
     await user.click(btn1)
-    await waitFor(()=>expect(getNodeText(size)).toEqual('10'))
+    await waitFor(() => expect(getNodeText(size)).toEqual('10'))
     expect(component).toMatchSnapshot()
   })
 })
